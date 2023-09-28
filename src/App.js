a2348d570653f9c40298773b0af0cbf1df79ca4e
Sebastian Vasquez <sebitas0489@gmail.com>
2023-09-27 20:51:38 -0500

    Maquetación: Estructura completa.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './platzi.webp';
+import React from 'react';
 import './App.css';
+
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const defaultTodos = [
+  { text: 'Iniciar Curso ReactJS', completed: true },
+  { text: 'Iniciar Curso Manipulación del DOM', completed: false },
+  { text: 'Practicar lo aprendido', completed: false },
+  { text: 'Finalizar Curso ReactJS', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter  completed={16} total={20}/>
+      <TodoSearch />
+
+        <TodoList>
+          {defaultTodos.map(todo => (
+            <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            />
+          ))}
+
+        </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
