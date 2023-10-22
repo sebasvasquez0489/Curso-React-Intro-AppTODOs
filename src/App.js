4b061b00995403d2579bf0f5aafc3a856a5097c1
Sebastian Vasquez <sebitas0489@gmail.com>
2023-10-22 14:58:07 -0500

    Corrección: Se extraen archivos de las carpetas para corregir error que presentaba con los modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { TodoCounter } from './components/TodoCounter/TodoCounter';
-import { TodoSearch } from './components/TodoSearch/TodoSearch';
-import { TodoList } from './components/TodoList/TodoList';
-import { TodoItem } from './components/TodoItem/TodoItem';
-import { CreateTodoButton } from './components/CreateTodoButton/CreateTodoButton';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 
 const defaultTodos = [
   { text: 'Iniciar Curso ReactJS', completed: true },
