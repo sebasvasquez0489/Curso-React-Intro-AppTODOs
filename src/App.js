020a2a046bf1396baca811eda65c8dea140790e3
Sebastian Vasquez <sebitas0489@gmail.com>
2023-10-24 23:00:43 -0500

    CutomHocks:Se implementa en una funcion aparte realizando la abstraccion de lo realizado en el localStorage.

--- src/App.js
+++ src/App.js
@@ -17,20 +17,32 @@ import { CreateTodoButton } from './CreateTodoButton';
 // stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
 
+//--Custo hooks-- Funcion para utilizar todo lo relacionado con "LocalStorage".--//
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+  const [item, setItem] = React.useState(parsedItem);
+
+    //--Funcion para actualizar el loclaStorage y el estado.--//
+    const saveItem = (newItem) => {
+      localStorage.setItem(itemName, JSON.stringify(newItem));
+        setItem(newItem);
+    };
+    return [item, saveItem];
+}
+
 function App() {
 
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
   //AGREGANDO ESTADOS
-  const [todos, setTodos] = React.useState(parsedTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('')
 
   //--Estado derivado para validación de cuantos TODOs se han completado--//
@@ -45,12 +57,6 @@ function App() {
       return todoText.includes(searchText);
     }
   );
-
-  //--Funcion para actualizar el loclaStorage--//
-  const saveTodos = (newTodos) => {
-    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-      setTodos(newTodos);
-  };
 
   //--Actualizador del estado--//
   const completeTodo = (text) => {
